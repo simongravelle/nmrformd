567399b62e14145a0b5fc963060c1bd566f6214c
Simon Gravelle <simon.gravelle@live.fr>
2022-01-21 22:55:26 +0100

    created class NMR and test file

--- nmrformd/__init__.py
+++ nmrformd/__init__.py
@@ -1,9 +1,27 @@
-#!/usr/bin/env python3
 
-def nmr(u:MDAnalysis.core.universe.Universe, 
-	target_i='H':str, 
-	neighbor_j='H':str, 
-	number_i=0:int, 
-	type_calcul='all':str):
-	
-	pass
+import numpy as np
+
+class NMR:
+    def __init__(self,
+                 u,
+                 target_i,
+                 neighbor_j,
+                 type_analysis,
+                 number_j):
+        self.u = u
+        self.target_i = target_i
+        self.neighbor_j = neighbor_j
+        self.type_analysis = type_analysis
+        self.number_j = number_j
+        self._read_universe()
+
+    def _read_universe(self):
+        self.group_target_i = self.u.select_atoms(self.target_i)
+        self.group_neighbor_j = self.u.select_atoms(self.neighbor_j)
+
+        print(self.group_target_i.atoms.n_atoms)
+        print(self.group_neighbor_j.atoms.n_atoms)
+
+
+
+
--- test/try.py
+++ test/try.py
@@ -1,5 +1,7 @@
 import MDAnalysis as mda
 u = mda.Universe('run.tpr', 'run.xtc')
+print(u.atoms.n_atoms)
 from nmrformd import NMR
 
 cal_nmr = NMR(u,'HW1','HW2',5)
+cal_nmr
