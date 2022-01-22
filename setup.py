db7b6dba43d4ae0fb6c09600b703641669aeaeb3
Simon Gravelle <simon.gravelle@live.fr>
2022-01-22 19:04:52 +0100

    corrected test

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='nmrformd',
-      version='0.1',
+      version='0.0.0',
       description='Calculate NMR relaxation time from molecular dynamics trajectory file',
-      long_description=open('README.md').read(),
+      long_description=open('README.rst').read(),
       url='https://github.com/simongravelle/nmrformd',
       author='Simon Gravelle',
       author_email='simon.gravelle@live.fr',
@@ -14,7 +14,6 @@ setup(name='nmrformd',
        "mdanalysis",
        "pytest",
        "numpy",
-       "random",
        "scipy",
       ]      
       )
