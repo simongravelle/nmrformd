fa171dc5be05731ef20703a4cf6d99f524c1c1a2
Simon Gravelle <sgravelle@icp.uni-stuttgart.de>
2022-01-28 17:15:02 +0100

    added v2 setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='nmrformd',
-      version='0.0.1',
+      version='v0.0.2',
       description='Calculate NMR relaxation time from molecular dynamics trajectory file',
       long_description=open('pypi/README.rst').read(),
       url='https://github.com/simongravelle/nmrformd',
-      download_url='https://github.com/simongravelle/nmrformd/archive/refs/tags/v0.0.1.tar.gz',
+      download_url='https://github.com/simongravelle/nmrformd/archive/refs/tags/v0.0.2.tar.gz',
       author='Simon Gravelle',
       author_email='simon.gravelle@live.fr',
       license='GNU GENERAL PUBLIC LICENSE',
