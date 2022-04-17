378d8b2c3c273fa6b9cf03fed11acf942f57e3a7
Simon Gravelle <simon.gravelle@live.fr>
2022-04-17 20:45:24 +0200

    corrected installation instruction git
    Former-commit-id: 92f06be24669a141c22a5f6d7dfee43f3ce7dc43

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='nmrformd',
-      version='v0.0.6',
+      version='v0.0.8',
       description='Calculate NMR relaxation time from molecular dynamics trajectory file',
       long_description=open('README.rst').read(),
       long_description_content_type='text/x-rst',
       url='https://github.com/simongravelle/nmrformd',
-      download_url='https://github.com/simongravelle/nmrformd/archive/refs/tags/v0.0.6.tar.gz',
+      download_url='https://github.com/simongravelle/nmrformd/archive/refs/tags/v0.0.8.tar.gz',
       author='Simon Gravelle',
       author_email='simon.gravelle@live.fr',
       license='GNU GENERAL PUBLIC LICENSE',
