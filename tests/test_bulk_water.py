056f972624211418ea5569f3e937f9143d6df148
Simon Grave;lle <simon.gravelle@live.fr>
2022-07-10 13:28:20 +0200

    improve testing

--- tests/test_bulk_water.py
+++ tests/test_bulk_water.py
@@ -11,7 +11,7 @@
 import MDAnalysis as mda
 import numpy as np
 
-import nmrformd as NMR
+import nmrformd as nmrmd
 
 
 def test_nmr():
@@ -19,19 +19,24 @@ def test_nmr():
     u = mda.Universe("bulk_water_lammps/conf.data",
                      "bulk_water_lammps/traj.xtc")
     ids = ""
-    for i in np.arange(1, 300, 6):
+    for i in np.arange(1, 300, 24):
         ids = ids + " " + str(i)
     group_i = "type 2 and index"+ids
     group_j = "type 2"
-    nmr_1 = NMR.NMR(u, [group_i, group_j],
+    nmr_1 = nmrmd.NMR(u, [group_i, group_j],
                     type_analysis="inter_molecular")
-    assert np.isclose(nmr_1.tau, 4.796872)
-    assert np.isclose(nmr_1.delta_omega, 38.22099)
-    assert np.isclose(nmr_1.T1, 6.50655)
+    assert np.isclose(nmr_1.tau, 4.66710)
+    assert np.isclose(nmr_1.delta_omega, 37.80930)
+    assert np.isclose(nmr_1.T1, 6.833889)
+    assert np.isclose(nmr_1.T2, 6.833889)
 
-    nmr_2 = NMR.NMR(u, [group_i, group_j],
+    nmr_2 = nmrmd.NMR(u, [group_i, group_j],
                     type_analysis="inter_molecular",
                     order="m012")
-    assert np.isclose(nmr_2.tau[0], 4.796872)
-    assert np.isclose(nmr_2.delta_omega[0], 38.22099)
-    assert np.isclose(nmr_2.T1, 6.99148)
+    assert np.isclose(nmr_2.tau[0], 4.667102)
+    assert np.isclose(nmr_2.delta_omega[0], 37.809309)
+    assert np.isclose(nmr_2.T1, 7.1193362)
+
+    nmr_3 = nmrmd.NMR(u, [group_i, group_j], f0=1e5)
+    assert np.isclose(nmr_3.T1, 11.42645)
+    assert np.isclose(nmr_3.T2, 3.677240)
