a3ab115a193aee636fd35ada1801be6a64a9e752
Simon Grave;lle <simon.gravelle@live.fr>
2022-07-22 19:26:22 +0200

    wrong assertion

--- nmrformd/NMR.py
+++ nmrformd/NMR.py
@@ -208,11 +208,13 @@ class NMR:
         elif self.type_analysis == "inter_molecular":
             _different_residue : bool = self.group_neighbor_j.resids != self._resids_i
             _index_j = self.group_neighbor_j.atoms.indices[_different_residue]
-            if not _index_j.any():
-                raise ValueError("Empty atom groups j, wrong combination of type_analysis"
-                                 "and group selection?")
+            # #tofix : wrong condition !
+            #if not _index_j.any():
+            #    raise ValueError("Empty atom groups j, wrong combination of type_analysis"
+            #                     "and group selection?")
             _str_j = ' '.join(str(e) for e in _index_j)
             self.group_j = self.u.select_atoms('index ' + _str_j)
+            
         elif self.type_analysis == "full":
             _different_atom : bool = self.group_neighbor_j.indices != self.index_i[self._cpt_i]
             _index_j = self.group_neighbor_j.atoms.indices[_different_atom]
