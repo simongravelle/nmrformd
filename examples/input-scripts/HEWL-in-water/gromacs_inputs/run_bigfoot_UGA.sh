2db00e2252919c645ca285b55e90b8842b5b8c08
Simon Gravelle <simon.gravelle@live.fr>
2023-07-31 22:29:12 +0200

    imrpoved HEWL
    Former-commit-id: 74287fb1de8f20584b9a4c38e483fee82b0334b8
    Former-commit-id: 069bf16e35d5e45cca1c233379d10a27ba40d7bc
    Former-commit-id: 6752111b34e5729b4afe4d5030d43fc56585b53a
    Former-commit-id: 4f236c7431008db4f25f5b829e8b97cfee331a04

--- examples/input-scripts/HEWL-in-water/gromacs_inputs/run_bigfoot_UGA.sh
+++ examples/input-scripts/HEWL-in-water/gromacs_inputs/run_bigfoot_UGA.sh
@@ -12,11 +12,14 @@ export GMX_MAXBACKUP=-1
 
 gmx=/home/gravells/softwares/gromacs-2023/build-gpu/bin/gmx
 
-${gmx} grompp -f input/nvt.mdp -o nvt -pp nvt -po nvt -c conf.gro
+${gmx} grompp -f input/min.mdp -o min -pp min -po min -c conf.gro
+${gmx} mdrun -deffnm min -v -rdd 1 -nt 8 -pin on
+
+${gmx} grompp -f input/nvt.mdp -o nvt -pp nvt -po nvt -c min.gro
 ${gmx} mdrun -deffnm nvt -v -rdd 1 -nt 8 -pin on
 
-${gmx} grompp -f input/npt.mdp -o npt -pp npt -po npt -c nvt.gro -maxwarn 1
+${gmx} grompp -f input/npt.mdp -o npt -pp npt -po npt -c nvt.gro
 ${gmx} mdrun -deffnm npt -v -rdd 1 -nt 8 -pin on
 
-${gmx} grompp -f input/prod.mdp -o prod -pp prod -po prod -c npt.gro -maxwarn 1
+${gmx} grompp -f input/prod.mdp -o prod -pp prod -po prod -c npt.gro
 ${gmx} mdrun -deffnm prod -v -rdd 1 -nt 8 -pin on
