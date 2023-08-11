329164a40bf28dcc3e258dfba37d133b907373e7
Simon Gravelle <simon.gravelle@live.fr>
2023-08-11 10:59:59 +0200

    improved data

--- examples/analyse-scripts/HEWL-in-water/run_dahu.sh
+++ examples/analyse-scripts/HEWL-in-water/run_dahu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #OAR -n nmrformd
-#OAR -l /nodes=1/cpu=1/core=1,walltime=48:00:00
+#OAR -l /nodes=1/cpu=1/core=1,walltime=12:00:00
 #OAR --stdout log.out
 #OAR --stderr log.err
 #OAR --project tamtam
