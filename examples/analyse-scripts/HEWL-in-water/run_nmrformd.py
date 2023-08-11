a68df850195dde93e667af527ba0eac893ce9f31
Simon Gravelle <simon.gravelle@live.fr>
2023-08-11 10:59:59 +0200

    improved data
    Former-commit-id: 329164a40bf28dcc3e258dfba37d133b907373e7
    Former-commit-id: 91b6ae509625da851d435e2e6ff89af4cd479f81
    Former-commit-id: ebcb4f7e60a52b5c27edf2e918d28b38d6523611
    Former-commit-id: 33c6a7fdc508231d844fcde8763fe5dbc4a8bf29

--- examples/analyse-scripts/HEWL-in-water/run_nmrformd.py
+++ examples/analyse-scripts/HEWL-in-water/run_nmrformd.py
@@ -26,12 +26,12 @@ for rho_water in [0.08, 0.17, 0.25, 0.39, 0.52, 0.73]:
     u = mda.Universe(datapath+folder+"prod.tpr", datapath+folder+"prod.xtc")
     h_all, h_water, h_protein = detect_groups(u)
 
-    # Calculate NMR properties for only one or 5 atoms at a time
+    # Calculate NMR properties for only one atom at a time
     nmr_h2o_intra = nmrmd.NMR(u, atom_group=h_water, neighbor_group=h_water, number_i=1, type_analysis="intra_molecular")
     nmr_h2o_inter = nmrmd.NMR(u, atom_group=h_water, neighbor_group=h_water, number_i=1, type_analysis="inter_molecular")
     nmr_pro_intra = nmrmd.NMR(u, atom_group=h_protein, neighbor_group=h_protein, number_i=1)
     nmr_h2o_pro = nmrmd.NMR(u, atom_group=h_water, neighbor_group=h_protein, number_i=1)
-    nmr_tot_tot = nmrmd.NMR(u, atom_group=h_all, neighbor_group=h_all, number_i=5)
+    nmr_tot_tot = nmrmd.NMR(u, atom_group=h_all, neighbor_group=h_all, number_i=1)
 
     save_nmr_data(filename, f_cut_off, nmr_h2o_intra, nmr_h2o_inter, nmr_pro_intra, nmr_h2o_pro, nmr_tot_tot)
 
