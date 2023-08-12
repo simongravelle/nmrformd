bd5e02984876de846f865ac8569456c3296e5f3f
Simon Gravelle <simon.gravelle@live.fr>
2023-08-12 09:40:26 +0200

    improved nmr data

--- examples/analyse-scripts/HEWL-in-water/run_dahu.sh
+++ examples/analyse-scripts/HEWL-in-water/run_dahu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #OAR -n nmrformd
-#OAR -l /nodes=1/cpu=1/core=1,walltime=12:00:00
+#OAR -l /nodes=1/cpu=1/core=1,walltime=48:00:00
 #OAR --stdout log.out
 #OAR --stderr log.err
 #OAR --project tamtam
 
 source /bettik/gravells/venv/bin/activate
 
-for i in {1..50}
+for i in {1..500}
 do
     python3 run_nmrformd.py
 done
