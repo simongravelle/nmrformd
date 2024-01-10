3bc3670b64f5c94239a7bd57f3c3ecbd90589ded
Simon Gravelle <simon.gravelle@live.fr>
2024-01-10 20:17:28 +0100

    added data

--- examples/illustrations/bulk-water-tip4p-vs-dumping/analysis.py
+++ examples/illustrations/bulk-water-tip4p-vs-dumping/analysis.py
@@ -19,37 +19,43 @@ R1_intra = 1e-5
 R1_inter = 1e-5
 T = 300
 while run:
-    for step in [2, 4, 8, 16, 32]:
+    for step in [1, 2, 4, 8, 16, 32, 64, 128, 256]:
+        # Import MDA universe
+        datapath = path + "N4000_" + str(T) + "K_HR/"
+        u = mda.Universe(datapath+"prod.tpr", datapath+"prod.xtc")
+        if step > 1:
+            u.transfer_to_memory(step = step)
+        dt = u.trajectory.dt
+
         # Read current status
-        name_intra = "N4000_intra_step" + str(step)
-        name_inter = "N4000_inter_step" + str(step)
+        name_intra = "N4000_intra_dt" + str(np.round(dt,5)) + "ps"
+        name_inter = "N4000_inter_dt" + str(np.round(dt,5)) + "ps"
         n_intra = measure_N(name_intra)
         n_inter = measure_N(name_inter)
-        # Import MDA universe
-        datapath = path + "N4000_" + str(T) + "K/"
-        u = mda.Universe(datapath+"prod.tpr", datapath+"prod.xtc")
-        u.transfer_to_memory(step = step)
-        hydrogen = u.select_atoms("name HW1 HW2")
-        n_hydrogen = hydrogen.n_atoms
-        if n_intra < n_hydrogen:
-            # Calculated NMR properties
-            intra = nmrformd.NMR(u, atom_group = hydrogen,
-                                 neighbor_group = hydrogen,
-                                 type_analysis = "intra_molecular",
-                                 number_i = 1)
-            save_result(intra, name = name_intra)
-            if np.random.random() < 0.2:
-                inter = nmrformd.NMR(u, atom_group = hydrogen,
-                                     neighbor_group = hydrogen,
-                                     type_analysis = "inter_molecular",
-                                     number_i = 1)
-                save_result(inter, name = name_inter)
-        else:
-            run = False
-        # Print information
-        R1_intra = measure_R1(name_intra)
-        R1_inter = measure_R1(name_inter)
-        print("step =", step, " --", n_intra, n_hydrogen, 
-            "R1:", np.round(R1_intra,2), np.round(R1_inter,2), 
-            "T1:", np.round(1/(R1_intra+R1_inter),2))
+
+        for _ in range(5):
+
+            hydrogen = u.select_atoms("name HW1 HW2")
+            n_hydrogen = hydrogen.n_atoms
+            if n_intra < n_hydrogen:
+                # Calculated NMR properties
+                intra = nmrformd.NMR(u, atom_group = hydrogen,
+                                    neighbor_group = hydrogen,
+                                    type_analysis = "intra_molecular",
+                                    number_i = 1)
+                save_result(intra, name = name_intra)
+                if np.random.random() < 0.2:
+                    inter = nmrformd.NMR(u, atom_group = hydrogen,
+                                        neighbor_group = hydrogen,
+                                        type_analysis = "inter_molecular",
+                                        number_i = 1)
+                    save_result(inter, name = name_inter)
+            else:
+                run = False
+            # Print information
+            R1_intra = measure_R1(name_intra)
+            R1_inter = measure_R1(name_inter)
+            print("dt =", np.round(dt,5), " ps --", n_intra, n_hydrogen, 
+                "R1:", np.round(R1_intra,2), np.round(R1_inter,2), 
+                "T1:", np.round(1/(R1_intra+R1_inter),2))
     print(" ")
