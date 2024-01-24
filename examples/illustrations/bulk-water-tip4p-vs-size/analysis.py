5b04b75041090a6f02529bedc533e7cdd6a111db
Simon Gravelle <simon.gravelle@live.fr>
2024-01-24 17:09:41 +0100

    improved convergence N figure

--- examples/illustrations/bulk-water-tip4p-vs-size/analysis.py
+++ examples/illustrations/bulk-water-tip4p-vs-size/analysis.py
@@ -18,8 +18,8 @@ run = True
 R1_intra = 1e-5
 R1_inter = 1e-5
 while run:
-    for N in [25, 39, 62, 99, 158, 251, 398, 631, 1002, 1589, 2521]:
-        for n in range(10):
+    for N in [25, 39, 62, 99, 158, 251, 398, 631, 1002, 1589, 2521, 6500, 10000]: 
+        for n in range(20):
             # Import MDA universe
             datapath = path + "N"+str(N)+"/"
             if os.path.exists(datapath+"prod-"+str(n)+".xtc"):
@@ -28,10 +28,14 @@ while run:
                 name_inter = "N"+str(N)+"_inter_n"+str(n) 
                 n_intra = measure_N(name_intra)
                 n_inter = measure_N(name_inter)
-                u = mda.Universe(datapath+"topology.data", datapath+"prod-"+str(n)+".xtc")
-                hydrogen = u.select_atoms("type 2")
+                try:
+                    u = mda.Universe(datapath+"topology.data", datapath+"prod-"+str(n)+".xtc")
+                    hydrogen = u.select_atoms("type 2")
+                except:
+                    u = mda.Universe(datapath+"prod.tpr", datapath+"prod-"+str(n)+".xtc")
+                    hydrogen = u.select_atoms("type HW")
                 n_hydrogen = hydrogen.n_atoms
-                if n_intra < n_hydrogen:
+                if n_intra < n_hydrogen*2:
                     # Calculated NMR properties
                     intra = nmrformd.NMR(u, atom_group = hydrogen,
                                         neighbor_group = hydrogen,
@@ -44,13 +48,12 @@ while run:
                                             type_analysis = "inter_molecular",
                                             number_i = 1)
                         save_result(inter, name = name_inter)
-                else:
-                    print("N", N, "no calculation")
-                    #run = False
-                # Print information
-                R1_intra = measure_R1(name_intra)
-                R1_inter = measure_R1(name_inter)
-                print("N =", N, "n", n, "----", n_intra, n_hydrogen, 
-                    "R1:", np.round(R1_intra,2), np.round(R1_inter,2), 
-                    "T1:", np.round(1/(R1_intra+R1_inter),2))
+                    # Print information
+                    R1_intra = measure_R1(name_intra)
+                    R1_inter = measure_R1(name_inter)
+                    print("N =", N, "n", n, "----", n_intra, n_hydrogen, 
+                        "R1:", np.round(R1_intra,2), np.round(R1_inter,2), 
+                        "T1:", np.round(1/(R1_intra+R1_inter),2))
+                #else:
+                #    print("N", N, "no calculation")
     print(" ")
